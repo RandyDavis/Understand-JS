0a0f5f9b2a34eb8febc2da7dca3572252d8b15fd
Randy Davis <randy_davis@outlook.com>
2015-08-14 00:21:04 -0500

    add operator precedence and associativity example

---
+++ section3-TypesAndOperators/C4_PrecedenceAndAssociativity/app.js
@@ -0,0 +1,16 @@
+// var a = 3 + 4 * 5;  
+// console.log(a);  // 23
+
+
+var a = 2, b = 3, c = 4;
+a = b = c;
+
+console.log(a); // 4
+console.log(b); // 4
+console.log(c); // 4
+
+b = c; // 4
+
+
+var d = (3 + 4) * 5;
+console.log(d); // 35
