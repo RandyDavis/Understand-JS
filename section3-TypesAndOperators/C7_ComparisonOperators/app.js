8e6f587770cb1e042454aa7a67efe5e1219271a2
Randy Davis <randy_davis@outlook.com>
2015-08-15 00:32:52 -0500

    begin comparison operators examples

---
+++ section3-TypesAndOperators/C7_ComparisonOperators/app.js
@@ -0,0 +1,10 @@
+console.log(1 < 2 < 3);  // true
+console.log(3 < 2 < 1);  // true
+
+
+var num = Number("3"), numF = Number(false), numT = Number(true);
+console.log(num); // 3
+console.log(numF); // 0
+console.log(numT); // 1
+
+
