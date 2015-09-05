f2ba99b48abc91a87ccb0e3d601ca7e03abdbed9
Randy Davis <randy_davis@outlook.com>
2015-09-05 10:12:47 -0500

    add gitignore and finishe comparison operator section

--- section3-TypesAndOperators/C7_ComparisonOperators/app.js
+++ section3-TypesAndOperators/C7_ComparisonOperators/app.js
@@ -8,3 +8,32 @@ console.log(numF); // 0
 console.log(numT); // 1
 
 
+var a = 0;
+var b = false;
+
+if (a == b) {
+    console.log('They are equal!'); // Returns this console message
+} else {
+    console.log("Nope, they are not equal!");
+}
+
+if (a === b) {
+    console.log('They are equal!');
+} else {
+    console.log("Nope, they are not equal!"); // Returns this console message
+}
+
+
+if (a != b) {
+    console.log('They are not equal!');
+} else {
+    console.log("Equal!"); // Returns this console message
+}
+
+
+
+if (a !== b) {
+    console.log('They are not equal!');  // Returns this console message
+} else {
+    console.log("Equal!");
+}
