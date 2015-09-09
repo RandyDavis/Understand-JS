75651ed5f5bfe83b2da99bf10d05c6908fc97052
Randy Davis <randy_davis@outlook.com>
2015-09-08 22:50:32 -0500

    add and complete whitespace section

---
+++ section4-ObjectsAndFunctions/D12_Whitespace/app.js
@@ -0,0 +1,22 @@
+var
+    // first name of the person
+    firstName,
+
+    // last name of the person
+    lastName,
+
+    // the languare
+    // can be 'en' or 'es'
+    language;
+
+var person = {
+    // the first name
+    firstName: 'Randy',
+
+
+    // the last name
+    // (always required)
+    lastName: 'Davis'
+}
+
+console.log(person);
