6929d576dc817cdc5ea051e1a7574fbb188bff03
Randy Davis <randy_davis@outlook.com>
2015-12-02 23:59:26 -0600

    complete strict mode lesson

---
+++ section7-OddsAndEnds/H3_Strictmode/app.js
@@ -0,0 +1,15 @@
+function logNewPerson() {
+    "use strict";
+
+    var person2;
+    persom2 = {};
+    console.log(persom2);
+}
+
+var person;
+// persom = {}; // persom is not defined error
+console.log(persom);
+logNewPerson();
+
+
+// Either put "use strict" at the very top of the page for everything to use it or you can put it at the top inside of a function for that function to use it
